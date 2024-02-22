e0f9e8748190c2f4b770f6957d5907f8949b2788
Chris <chrishode2015@gmail.com>
2024-02-21 22:58:06 -0500

    project 1 repost

---
+++ .vscode/Policy.java
@@ -0,0 +1,89 @@
+class Policy {
+    private String policyNumber;
+    private String providerName;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String smokingStatus;
+    private double height;
+    private double weight;
+
+    // getters
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSmokingStatus() {
+        return smokingStatus;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Policy() {
+        this.policyNumber = "";
+        this.providerName = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
+        this.smokingStatus = "";
+        this.height = 0.0;
+        this.weight = 0.0;
+    }
+
+    public Policy(String policyNumber, String providerName, String firstName, String lastName,
+                  int age, String smokingStatus, double height, double weight) {
+        this.policyNumber = policyNumber;
+        this.providerName = providerName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.smokingStatus = smokingStatus;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // bmi stuff
+    public double calculateBMI() {
+        return (weight * 703) / (height * height);
+    }
+
+    // nsurance policy price
+    public double calculatePolicyPrice() {
+        double bmi = calculateBMI();
+        double additionalFee = (bmi - 35) * 20;
+        double baseFee = 600;
+
+        if (age > 50) {
+            additionalFee += 75;
+        }
+
+        if (smokingStatus.equalsIgnoreCase("smoker")) {
+            additionalFee += 100;
+        }
+
+        return baseFee + additionalFee;
+    }
+}
